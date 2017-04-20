c42df6141574d4dee3c459b9f1201965ea99d1f6
Ken Ono <ken.ono.gm@gmail.com>
2017-04-20 18:01:13 -0400

    connection status and change buttons

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {Store} from '@ngrx/store';
+import {Observable} from 'rxjs/Observable';
+import {IAppState} from '../../store/app-state';
+import {CONNECT, DISCONNECT} from '../../store/connected.store';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  connected: Observable<boolean>;
+  constructor(
+    public navCtrl: NavController,
+    private store: Store<IAppState>
+  ) {
+    this.connected = store.select('connected');
+  }
 
-  constructor(public navCtrl: NavController) {
+  connect(){
+    this.store.dispatch({ type: CONNECT });
+  }
 
+  disconnect(){
+    this.store.dispatch({ type: DISCONNECT });
   }
 
 }
