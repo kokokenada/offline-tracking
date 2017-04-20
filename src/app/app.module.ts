873999f901551f453f15ae0685ea96e5f66666ce
Ken Ono <ken.ono.gm@gmail.com>
2017-04-20 18:03:02 -0400

    configure redux-beacon, off line and and the logger

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,57 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Action, StoreModule } from '@ngrx/store';
+import { storeLogger } from "ngrx-store-logger";
+import { connectReducer } from '../store/connected.store';
+import { navigateReducer } from '../store/navigation.store';
+import { IAppState } from '../store/app-state';
+
+// redux-beacon
+import { GoogleAnalytics, Event, PageView } from 'redux-beacon/targets/google-analytics';
+import { logger } from 'redux-beacon/extensions/logger';
+import { createMetaReducer } from 'redux-beacon';
+import { offlineWeb } from 'redux-beacon/extensions/offline-web';
+
+
+// (redux-beacon)
+const eventsMap = {
+  CONNECT: {
+    eventFields: (action: Action): Event => ({
+      hitType: 'event',
+      eventCategory: 'connected',
+      eventAction: action.type,
+    }),
+  },
+  DISCONNECT: {
+    eventFields: (action: Action): Event => ({
+      hitType: 'event',
+      eventCategory: 'disconnected',
+      eventAction: action.type,
+    }),
+  },
+  NAVIGATE: {
+    eventFields: (action: Action): PageView => ({
+      hitType: 'pageview',
+      page: action.payload,
+    }),
+  },
+};
+
+// (redux-beacon)
+const analyticsMetaReducer = createMetaReducer(eventsMap, GoogleAnalytics, { logger });
+// A meta reducer is just a function that takes in a reducer, and spits out
+// an augmented reducer
+
+const loggerMetaReducer = storeLogger();
+const isConnected = (state:IAppState) => {
+  console.log('isConnected')
+  console.log(state)
+  return state.connected;
+};
+
+// pass in the connectivity selector as the first parameter
+const offlineStorage = offlineWeb(isConnected);
 
 @NgModule({
   declarations: [
@@ -21,7 +72,11 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    StoreModule.provideStore({
+      connected: loggerMetaReducer(analyticsMetaReducer(connectReducer, offlineStorage)),
+      location: navigateReducer,
+    })
   ],
   bootstrap: [IonicApp],
   entryComponents: [
