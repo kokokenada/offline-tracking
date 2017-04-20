3a32b4672efce585c64a864578896fe3790cd86c
Ken Ono <ken.ono.gm@gmail.com>
2017-04-20 17:53:42 -0400

    fire nav event on view change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,38 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import {Component, ViewChild} from '@angular/core';
+import {Nav, Platform} from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { TabsPage } from '../pages/tabs/tabs';
+import {NAVIGATE} from '../store/navigation.store';
+import {IAppState} from '../store/app-state';
+import {Store} from '@ngrx/store';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage:any = TabsPage;
+  @ViewChild(Nav) nav:Nav;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(
+    platform: Platform,
+    statusBar: StatusBar,
+    splashScreen: SplashScreen,
+    private store: Store<IAppState>)
+  {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+
+      this.nav.viewDidEnter.subscribe(
+        view => {
+          this.store.dispatch({type: NAVIGATE, payload: view.name});
+        }
+      )
+
     });
   }
 }
