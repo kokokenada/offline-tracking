ef8ddbf0dc705642c2c9f506a1f2cc3a9a5cfe03
Ken Ono <ken.ono.gm@gmail.com>
2017-04-20 17:53:08 -0400

    fire nav events on tab changes

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -3,6 +3,9 @@ import { Component } from '@angular/core';
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
+import {IAppState} from '../../store/app-state';
+import {Store} from '@ngrx/store';
+import {NAVIGATE} from '../../store/navigation.store';
 
 @Component({
   templateUrl: 'tabs.html'
@@ -13,7 +16,13 @@ export class TabsPage {
   tab2Root = AboutPage;
   tab3Root = ContactPage;
 
-  constructor() {
+  constructor(
+    private store: Store<IAppState>
+  ) {
 
+  }
+
+  captureTabChange(event) {
+    this.store.dispatch({type: NAVIGATE, payload: event.tabTitle});
   }
 }
